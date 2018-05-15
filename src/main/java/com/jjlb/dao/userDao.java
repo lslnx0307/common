3ca362e875afded3978ad61c74a1171574f29994
lslnx <lishiliang@jinjilibao.com>
2018-05-15 13:37:12 +0800

    提交

---
+++ src/main/java/com/jjlb/dao/userDao.java
@@ -0,0 +1,17 @@
+package com.jjlb.dao;
+
+import com.jjlb.model.User;
+
+public interface userDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
